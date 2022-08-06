fb5551713c4bd02716c4fb31aeb92d47e85a8d39
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2022-08-06 18:21:41 -0400

    Auto play and muted added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import MediaPlayer from "./utils/MediaPlayer.js";
+import AutoPlay from "./utils/AutoPlay.js";
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
 const playPauseButton = document.querySelector('.play-pause');
 playPauseButton.onclick = () => player.togglePlay();
