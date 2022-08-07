3ce9d71cdc457718a3de0c56c11c4f2b1741f30e
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2022-08-07 09:12:51 -0400

    Pause if not visible

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import MediaPlayer from "./utils/MediaPlayer.js";
-import AutoPlay from "./utils/AutoPlay.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ 
+  el: video, 
+  plugins: [ new AutoPlay(), new AutoPause()] 
+});
 
 const playPauseButton = document.querySelector('.play-pause');
 playPauseButton.onclick = () => player.togglePlay();
